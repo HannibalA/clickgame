5a8dc8d7014a9cbd49ea60fd86725e9d743e80d3
HannibalA <Alawode.Hannibal@gmail.com>
2015-11-17 15:42:31 -0500

    fixed

--- js/javascript.js
+++ js/javascript.js
@@ -2,5 +2,13 @@ var clicks = 0;
     function count() {
         clicks += 1 < 12;
         document.getElementById("clicks").innerHTML = clicks;
-    };
+    }
 
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("You clicked " +clicks+ " Naruto characters !");
+     
+     }, 20000);
+
+}
